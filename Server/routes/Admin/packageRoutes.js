374eac045d7c17c9b2276ccc64629a09f9878218
sanikack <sanikack048@gmail.com>
2026-02-21 16:12:24 +0530

    file large problem

--- Server/routes/Admin/packageRoutes.js
+++ Server/routes/Admin/packageRoutes.js
@@ -8,18 +8,15 @@ router.get("/", GetPackages);
 router.post("/", (req,res,next) =>{
     console.log("ROUTE HIT");
     
-    Upload.fields([
-    {name:"image", maxCount:1},
-    {name:"gallery", maxCount:10},
-    
-])(req,res,function(err){
+},
+
+(err,req,res,next)=>{
     if(err){
         console.log("MULTER ERROR:", err.message);
-        return res.status(400).json({ error: err.message });
-        
+      return res.status(400).json({ error: err.message });
     }
+
     next()
-})
 },
  CreatePackage )
 
@@ -27,15 +24,8 @@ router.get("/:id", GetSinglePackage );
 
 router.put("/:id", Upload.fields([
     {name:"image", maxCount:1},
-    {name:"gallery", maxCount:10},
-])(req,res,function(err){
-    if(err){
-        console.log("MULTER ERROR:", err.message);
-        
-    }
-    next()
-}),
- UpdatePackage)
+    {name:"gallery", maxCount:10}
+]), UpdatePackage)
 
 router.delete("/:id", DeletePackge)
 
