8d69a14e5bf93ecfc0cd75c4917d52508678e413
sanikack <sanikack048@gmail.com>
2026-02-22 23:46:18 +0530

    try to solve destination problem

--- Server/server.js
+++ Server/server.js
@@ -44,7 +44,8 @@ app.use(cors({
 console.log(process.env.FRONTEND_URL);
 
 
-app.use(express.json())
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({limit: "50mb", extended:true}))
 app.use(cookieParser())
 app.use("/uploads", express.static("uploads"))
 
