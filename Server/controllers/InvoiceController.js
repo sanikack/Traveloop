1bafcb33db594a9d5b7d4b2849ea34695139bd55
sanikack <sanikack048@gmail.com>
2026-02-24 12:14:04 +0530

    Switch to puppeteer-core for render compatibility

--- Server/controllers/InvoiceController.js
+++ Server/controllers/InvoiceController.js
@@ -1,4 +1,4 @@
-const puppeteer= require("puppeteer");
+const puppeteer= require("puppeteer-core");
 const ejs= require("ejs");
 const path = require("path");
 const BookingSchema= require("../models/BookingSchema");
@@ -38,8 +38,14 @@ const DownloadInvoice= async (req,res)=>{
     const html= await ejs.renderFile(TempPath, {booking});
 
     // best for windows and chrome
-    const browser = await puppeteer.launch({ headless:"new", 
-      args:["--no-sandbox", "--disable-setuid-sandbox"]
+    const browser = await puppeteer.launch({ 
+      executablePath: "/usr/bin/chromium-browser",
+      headless:"new", 
+      args:["--no-sandbox", 
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-gpu",
+      ]
      });
 
     const page = await browser.newPage();
